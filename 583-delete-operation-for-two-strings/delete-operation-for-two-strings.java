649a14af8ceb6126dd32339ac4c5658de81e85ce
adarshtiwari908 <adarshtiwari908@gmail.com>
2025-07-01 13:18:34 +0530

    Time: 6 ms (94.15%) | Memory: 44.9 MB (36.45%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -3,7 +3,6 @@ public class Solution {
         int m = word1.length();
         int n = word2.length();
 
-        // Step 1: Find LCS length
         int[][] dp = new int[m + 1][n + 1];
 
         for (int i = 1; i <= m; i++) {
