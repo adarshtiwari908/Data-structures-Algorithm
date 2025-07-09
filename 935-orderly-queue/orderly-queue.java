010243a4ef8a4a7bee9917f73e4b2c141c6b7c4c
adarshtiwari908 <adarshtiwari908@gmail.com>
2025-07-09 19:37:16 +0530

    Time: 3 ms (92.93%) | Memory: 44.8 MB (45.20%) - LeetSync

--- 935-orderly-queue/orderly-queue.java
+++ 935-orderly-queue/orderly-queue.java
@@ -1,20 +1,23 @@
+import java.util.Arrays;
+
 class Solution {
     public String orderlyQueue(String s, int k) {
-        if (k == 1) {
-            String min = s;
-            for (int i = 1; i < s.length(); i++) {
-                // rotate by moving first i characters to the end
-                String rotated = s.substring(i) + s.substring(0, i);
-                if (rotated.compareTo(min) < 0) {
-                    min = rotated;
-                }
-            }
-            return min;
-        } else {
-            // if k >= 2, just sort the string
+        if (k > 1) {
+
             char[] chars = s.toCharArray();
             Arrays.sort(chars);
             return new String(chars);
         }
+
+        String result = s;
+
+        for (int i = 1; i < s.length(); i++) {
+            String temp = s.substring(i) + s.substring(0, i);
+            if (temp.compareTo(result) < 0) {
+                result = temp;
+            }
+        }
+
+        return result;
     }
 }
