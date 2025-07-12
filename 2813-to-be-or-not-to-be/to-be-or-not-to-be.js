9336ca9d0403c2aaadd223b43f7a5bbd525cae6d
adarshtiwari908 <adarshtiwari908@gmail.com>
2025-07-12 21:09:40 +0530

    Time: 24 ms (99.68%) | Memory: 53.6 MB (44.32%) - LeetSync

---
+++ 2813-to-be-or-not-to-be/to-be-or-not-to-be.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: function(val2) {
+            if (val === val2) {
+                return true;
+            } else {
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: function(val2) {
+            if (val !== val2) {
+                return true;
+            } else {
+                throw new Error("Equal");
+            }
+        }
+    };
+};
