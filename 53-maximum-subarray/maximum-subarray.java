b8a0270781b891976ea7c22e5a401c320534861c
adarsh tiwari <adarshtiwari908@gmail.com>
2025-07-20 22:56:03 +0530

    Time: 1 ms (99.52%) | Memory: 56.9 MB (81.39%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.java
@@ -0,0 +1,12 @@
+class Solution{
+    public int maxSubArray(int[] nums){
+        int localMax = nums[0];
+        int globalMax = nums[0];
+
+        for(int i = 1; i<nums.length; i++){
+            localMax = Math.max(nums[i], localMax + nums[i]);
+            globalMax = Math.max(globalMax, localMax);
+        }
+        return globalMax;
+    }
+}
