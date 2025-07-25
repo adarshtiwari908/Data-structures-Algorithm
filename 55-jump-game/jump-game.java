58dff32eba0d787918c89625fc5ed6165a0e0174
adarsh tiwari <adarshtiwari908@gmail.com>
2025-07-26 01:05:05 +0530

    Time: 3 ms (50.65%) | Memory: 45.8 MB (55.73%) - LeetSync

--- 55-jump-game/jump-game.java
+++ 55-jump-game/jump-game.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean canJump(int[] nums) {
+/* m is maxReachable point and example we've simply [2,3,1,1,4].
+so, at first index we have 2 and that is our max... and so on as we loop through array*/
         int m = 0;
         for(int i = 0; i < nums.length; i++){
-            if( i > m )return false;
+            if( i > m )return false; //case 2 given in question already i is the where we want to reach
             else m = Math.max(m, i + nums[i]);
         }
         return true;
