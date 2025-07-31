f2f4979ad0ecb001867ccde6088ba5a0835eca4b
adarsh tiwari <adarshtiwari908@gmail.com>
2025-07-31 13:34:57 +0530

    Time: 1 ms (99.50%) | Memory: 44 MB (79.72%) - LeetSync

--- 443-string-compression/string-compression.java
+++ 443-string-compression/string-compression.java
@@ -1,23 +1,20 @@
 class Solution {
     public int compress(char[] chars) {
         int n = chars.length;
-        int index = 0;  // pointer to write compressed characters
-        int i = 0;      // pointer to read characters
+        int i = 0;
+        int index = 0;
 
         while (i < n) {
-            char curr_char = chars[i];  // current character
+            char currChar = chars[i];
             int count = 0;
 
-            // Count how many times curr_char repeats
-            while (i < n && chars[i] == curr_char) {
+            while (i < n && chars[i] == currChar) {
                 count++;
                 i++;
             }
 
-            // Write the character
-            chars[index++] = curr_char;
+            chars[index++] = currChar;
 
-            // If count > 1, write the digits
             if (count > 1) {
                 String countStr = String.valueOf(count);
                 for (char c : countStr.toCharArray()) {
@@ -26,6 +23,6 @@ class Solution {
             }
         }
 
-        return index;  // final compressed length
+        return index;
     }
 }
