8b7f198a110821a921e78a8b616ddcb9f1aacdc3
adarsh tiwari <adarshtiwari908@gmail.com>
2025-08-05 13:49:29 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (61.45%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -15,9 +15,14 @@
  */
 class Solution {
     public int maxDepth(TreeNode root) {
-        if (root == null) return 0;
+        if( root == null ) return 0;
         int leftDepth = maxDepth(root.left);
         int rightDepth = maxDepth(root.right);
         return Math.max(leftDepth, rightDepth) + 1;
     }
 }
+
+/*if the root is null, it means the tree is empty, so depth is 0.
+Recursively calculate the depth of the left and right subtrees.
+Return the maximum of both depths, and add 1 for the current node.
+*/
