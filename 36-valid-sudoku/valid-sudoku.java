1e03948bee88b5e110e487bef4aacd4c9631bf56
adarsh tiwari <adarshtiwari908@gmail.com>
2025-08-07 12:26:42 +0530

    Time: 14 ms (15.75%) | Memory: 45.2 MB (8.09%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -7,7 +7,7 @@ class Solution {
                 if(current_val != '.'){
                     if( !seen.add(current_val + "found in row" + i) ||
                         !seen.add(current_val + "found in col" + j) ||
-                        !seen.add(current_val + "found in sub box" + i/3 + " " + j/3))
+                        !seen.add(current_val + "found in sub box" + (i/3) + " " + (j/3)))
                         return false;
                 }
             }
