87f8d46ba4b226c0e5598e775392c63dbbc11b2d
adarsh tiwari <adarshtiwari908@gmail.com>
2025-08-12 18:17:10 +0530

    Time: 0 ms (100.00%) | Memory: 49.4 MB (36.45%) - LeetSync

--- 344-reverse-string/reverse-string.java
+++ 344-reverse-string/reverse-string.java
@@ -3,13 +3,12 @@ class Solution {
         int left = 0;
         int right = s.length - 1;
         while(left < right){
-        char temp = s[left];
-        s[left] = s[right];
-        s[right] = temp;
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
 
-        left++;
-        right--;
+            left++;
+            right--;
         }
     }
 }
- 
