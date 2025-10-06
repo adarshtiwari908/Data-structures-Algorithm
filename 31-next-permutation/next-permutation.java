27aa50a0505b48081d9ea566e2dd0d3a7ffb136c
adarsh tiwari <adarshtiwari908@gmail.com>
2025-10-06 13:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (75.97%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,35 +1,39 @@
 class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
+        
+        // 1. Find the pivot (first decreasing element from the right)
         int i = n - 2;
-
-        // Step 1: Find the first i from right such that nums[i] < nums[i + 1]
         while (i >= 0 && nums[i] >= nums[i + 1]) {
             i--;
         }
-
+        
+        // If a pivot is found (i >= 0)
         if (i >= 0) {
-            // Step 2: Find the number just greater than nums[i]
+            // 2. Find the swap element (smallest element greater than nums[i] in the suffix)
             int j = n - 1;
-            while (nums[j] <= nums[i]) {
+            while (j >= 0 && nums[j] <= nums[i]) {
                 j--;
             }
+            // Swap nums[i] and nums[j]
             swap(nums, i, j);
         }
-
-        // Step 3: Reverse the part from i+1 to end
+        
+        // 3. Reverse the suffix from i + 1 to the end
         reverse(nums, i + 1, n - 1);
     }
-
+    
     private void swap(int[] nums, int i, int j) {
-        int tmp = nums[i];
+        int temp = nums[i];
         nums[i] = nums[j];
-        nums[j] = tmp;
+        nums[j] = temp;
     }
-
-    private void reverse(int[] nums, int left, int right) {
-        while (left < right) {
-            swap(nums, left++, right--);
+    
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
         }
     }
 }
