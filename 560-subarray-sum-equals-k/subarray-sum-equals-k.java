99ab0f163f08849fb627269f79458987be1e2605
adarsh tiwari <adarshtiwari908@gmail.com>
2025-10-08 11:31:50 +0530

    Time: 1092 ms (14.48%) | Memory: 46.4 MB (76.33%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,26 +1,16 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public int subarraySum(int[] nums, int k) {
         int count = 0;
-        int sum = 0;
-
-        // Map to store (prefixSum, frequency)
-        HashMap<Integer, Integer> map = new HashMap<>();
-        map.put(0, 1); // base case: sum = 0 occurs once
-
-        for (int num : nums) {
-            sum += num; // running sum
-
-            // check if (sum - k) exists in map
-            if (map.containsKey(sum - k)) {
-                count += map.get(sum - k);
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            int currentSum = 0; 
+            for (int j = i; j < n; j++) {
+                currentSum = currentSum + nums[j]; 
+                if (currentSum == k) {
+                    count++;
+                }
             }
-
-            // update the frequency of current sum in map
-            map.put(sum, map.getOrDefault(sum, 0) + 1);
         }
-
         return count;
     }
 }
